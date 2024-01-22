ab99e40f0f4cdd486d7a3e818f53889b35291be6
marginalfriend <abdullahpsg@gmail.com>
2024-01-22 14:42:42 +0700

    cashin mapping -> status validation resolve

--- app/dashboard/actions.ts
+++ app/dashboard/actions.ts
@@ -30,14 +30,22 @@ const cashout = await prisma.cashflow.findMany({
 
 export const expenses: number = cashout
   .slice()
-  .filter((cashout) => cashout.status?.value === "paid")
+  .filter(
+    (cashout) =>
+      JSON.stringify(cashout.status) ===
+      JSON.stringify({ label: "Paid", value: "paid" }),
+  )
   .reduce(function (prev, next) {
     return prev + next.value;
   }, 0);
 
 export const income: number = cashin
   .slice()
-  .filter((cashin) => cashin.status?.value === "paid")
+  .filter(
+    (cashin) =>
+      JSON.stringify(cashin.status) ===
+      JSON.stringify({ label: "Paid", value: "paid" }),
+  )
   .reduce(function (prev, next) {
     return prev + next.value;
   }, 0);
@@ -46,14 +54,24 @@ export const balance: number = income - expenses;
 
 export const debt: number = cashout
   .slice()
-  .filter((cashout) => cashout.status?.value === "pending")
+  .filter(
+    (cashout) =>
+      JSON.stringify(cashout.status) ===
+      JSON.stringify({ label: "Pending", value: "pending" }),
+  )
   .reduce(function (prev, next) {
     return prev + next.value;
   }, 0);
 
 export const accountReceivable: number = cashin
   .slice()
-  .filter((cashin) => cashin.status?.value === "pending")
+  .filter(
+    (cashin) =>
+      JSON.stringify(cashin.status) ===
+      JSON.stringify({ label: "Pending", value: "pending" }),
+  )
   .reduce(function (prev, next) {
     return prev + next.value;
   }, 0);
+
+console.log(cashin);
