e4cf86f3731bfedfb2aea06b103584e8437d54b2
marginalfriend <abdullahpsg@gmail.com>
2024-01-28 10:40:51 +0700

    data table sorting and pagination

--- app/dashboard/cashflow-tables/components/columns.tsx
+++ app/dashboard/cashflow-tables/components/columns.tsx
@@ -3,6 +3,17 @@
 import { ColumnDef } from "@tanstack/react-table";
 import type { StatusType } from "../../components/status";
 import { Prisma } from "@prisma/client";
+import { ArrowUpDown, MoreHorizontal } from "lucide-react";
+
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 
 export type CashlflowTable = {
   id: string;
@@ -15,7 +26,17 @@ export type CashlflowTable = {
 export const columns: ColumnDef<CashlflowTable>[] = [
   {
     accessorKey: "value",
-    header: () => <div className="text-left">Amount</div>,
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Amount
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
     cell: ({ row }: { row: any }) => {
       const amount = parseInt(row.getValue("value"));
       const formatted = new Intl.NumberFormat("en-US", {
@@ -28,14 +49,34 @@ export const columns: ColumnDef<CashlflowTable>[] = [
   },
   {
     accessorKey: "subject",
-    header: () => <div className="text-left">Subject</div>,
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Subject
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
     cell: ({ row }) => {
       return <div className="text-left">{row.getValue("subject")}</div>;
     },
   },
   {
     accessorKey: "status",
-    header: "Status",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Status
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
     cell: ({ row }) => {
       const status: StatusType = row.getValue("status");
       const label = status.label;
@@ -44,7 +85,17 @@ export const columns: ColumnDef<CashlflowTable>[] = [
   },
   {
     accessorKey: "date",
-    header: "Tx Date",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Tx Date
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
     cell: ({ row }) => {
       const date = new Date(row.getValue("date")).toLocaleDateString("en-US", {
         day: "numeric",
@@ -53,6 +104,34 @@ export const columns: ColumnDef<CashlflowTable>[] = [
       });
 
       return <div className="text-left">{date}</div>;
+    },
+  },
+  {
+    id: "actions",
+    cell: ({ row }) => {
+      const payment = row.original;
+
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+              onClick={() => navigator.clipboard.writeText(payment.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
     },
   },
 ];
