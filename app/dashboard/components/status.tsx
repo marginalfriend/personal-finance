d44299922e2e52a5054d6aacd40c2c7c2d07fc33
marginalfriend <abdullahpsg@gmail.com>
2024-02-01 15:24:07 +0700

    dropdown menu fixed

--- app/dashboard/components/status.tsx
+++ app/dashboard/components/status.tsx
@@ -1,18 +1,13 @@
 "use client";
 
-import {
-  Popover,
-  PopoverTrigger,
-  PopoverContent,
-} from "../../../components/ui/popover";
 import { Check, ChevronsUpDown } from "lucide-react";
 import { Button } from "../../../components/ui/button";
 import {
-  Command,
-  CommandEmpty,
-  CommandGroup,
-  CommandItem,
-} from "../../../components/ui/command";
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { cn } from "@/lib/utils";
 import { useState } from "react";
 
@@ -40,44 +35,33 @@ export function Status({ data, sendData }: { data?: any; sendData: Function }) {
   };
 
   return (
-    <Popover open={open} onOpenChange={setOpen}>
-      <PopoverTrigger asChild>
-        <Button
-          variant="outline"
-          role="combobox"
-          aria-expanded={open}
-          className="w-[100px] justify-between"
-        >
+    <DropdownMenu>
+      <DropdownMenuTrigger asChild>
+        <Button variant="outline" className="w-[100px] justify-between">
           {value
             ? status.find((status) => status.value === value)?.label
             : "+ Status"}
           <ChevronsUpDown className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
-      </PopoverTrigger>
-      <PopoverContent className="w-[100px]  p-0">
-        <Command>
-          <CommandEmpty>No status found</CommandEmpty>
-          <CommandGroup>
-            {status.map((status) => (
-              <CommandItem
-                key={status.value}
-                value={status.value}
-                onSelect={(currentValue) => {
-                  handleSelect(currentValue);
-                }}
-              >
-                <Check
-                  className={cn(
-                    "mr-2 h-4 w-4",
-                    value === status.value ? "opacity-100" : "opacity-0",
-                  )}
-                />
-                {status.label}
-              </CommandItem>
-            ))}
-          </CommandGroup>
-        </Command>
-      </PopoverContent>
-    </Popover>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        {status.map((status) => (
+          <DropdownMenuItem
+            key={status.value}
+            onSelect={() => {
+              handleSelect(status.value);
+            }}
+          >
+            <Check
+              className={cn(
+                "mr-2 h-4 w-4",
+                value === status.value ? "opacity-100" : "opacity-0",
+              )}
+            />
+            {status.label}
+          </DropdownMenuItem>
+        ))}
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
